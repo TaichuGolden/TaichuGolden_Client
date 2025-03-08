d6274257367e9974845b0b25c935c33c7b734268
jiujiuyao <jiujiuwhoami@outlook.com>
2025-03-08 21:12:00 +0800

    update

--- MacdCross/MacdCross.py
+++ MacdCross/MacdCross.py
@@ -1,37 +1,39 @@
-async def loop(base):
-    try:
-        if len(base.data) <= 100:
-            return 
-        ind = base.ind
-        tools = base.tools
-        macd, signal, hist = ind.macd(
-            data=base.data['CLOSE'], 
-            fastperiod=12, 
-            slowperiod=26, 
-            signalperiod=9
-            ) 
-        cross = tools.cross(macd, signal)
-        if cross > 0:
-            base.open(
-                side='LONG',
-                quantity=1,
-                order_type='limit',
-                price=base.data['CLOSE'].iloc[-1],
-                send_msg=True,
-                timestamp=base.data.index[-1],
-                timeout='1d',
-                additional_params=None
-            )
-        if cross < 0:
-            base.open(
-                side='SHORT',
-                quantity=1,
-                order_type='limit',
-                price=base.data['CLOSE'].iloc[-1],
-                send_msg=True,
-                timestamp=base.data.index[-1],
-                timeout='1d',
-                additional_params=None
-            )
-    except Exception as e:
-        raise e
+
+class MacdCross:
+    def __init__(self,base):
+        base.balance(cash=500000,leverage=10,fee_rate=0.00015,margin_rate=0.1)
+    async def on_tick(self,base):
+        try:
+            if len(base.data) <= 100:
+                return 
+            macd, signal, hist = base.ind.macd(
+                data=base.data['CLOSE'], 
+                fastperiod=12, 
+                slowperiod=26, 
+                signalperiod=9
+                ) 
+            cross = base.tools.cross(macd, signal)
+            if cross > 0:
+                base.open(
+                    side='LONG',
+                    quantity=1,
+                    order_type='limit',
+                    price=base.data['CLOSE'].iloc[-1],
+                    send_msg=True,
+                    timestamp=base.data.index[-1],
+                    timeout='1d',
+                    additional_params=None
+                )
+            if cross < 0:
+                base.open(
+                    side='SHORT',
+                    quantity=1,
+                    order_type='limit',
+                    price=base.data['CLOSE'].iloc[-1],
+                    send_msg=True,
+                    timestamp=base.data.index[-1],
+                    timeout='1d',
+                    additional_params=None
+                )
+        except Exception as e:
+            raise e
